940632f2226db57af275e8a426ef1784a25c9c71
samdahee27 <sdeheer@arcadia.edu>
2024-12-03 15:46:51 -0500

    Update deck.java

--- src/deck.java
+++ src/deck.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        for (int suit = 0; suit < 4; suit++) {
+            for (int value = 1; value <= 13; value++) {
+                cards.add(new Card(value, suit));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card deal() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        } else {
+            return null; // No cards left to deal
+        }
+    }
+
+    public int size() {
+        return cards.size();
+    }
+}
