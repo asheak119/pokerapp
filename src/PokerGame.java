fcd6c690f94f02376338aad7cd12c0bcf8701519
samdahee27 <sdeheer@arcadia.edu>
2024-12-04 14:19:47 +0000

    Start of PokerGame

--- src/PokerGame.java
+++ src/PokerGame.java
@@ -1,6 +1,33 @@
-import java.util.*;
-
 public class PokerGame {
+    private Deck deck;
+    private Hand[] playerHands;
+    private int numPlayers;
 
+    public PokerGame(int numPlayers) {
+        this.numPlayers = numPlayers;
+        this.deck = new Deck();
+        this.playerHands = new Hand[numPlayers];
+    }
 
+    public void startGame() {
+        if (numPlayers <= 0) {
+            System.out.println("No players in the game!");
+            return; // Exit early if there are no players
+        }
+
+        deck.shuffle();
+        System.out.println("Deck shuffled!");
+    
+        // Deal hands to players
+        for (int i = 0; i < numPlayers; i++) {
+            Card[] cards = new Card[5];
+            for (int j = 0; j < 5; j++) {
+                cards[j] = deck.deal();
+            }
+            playerHands[i] = new Hand(cards);
+            System.out.println("Player " + (i + 1) + "'s hand: " + playerHands[i]);
+        }
+    
+    }
+    
 }
