406a45cb84a7b7bd2da4d6f438df4d614ce8017a
YahanXu629 <yxu_03@arcadia.edu>
2024-12-08 20:11:06 -0500

    Update Player.java
    small changes prevent wrong input

--- src/Player.java
+++ src/Player.java
@@ -16,18 +16,30 @@ public class Player {
         return chips;
     }
     public void bet(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Bet amount must be positive!");
+        }
         if (amount > chips) {
             throw new IllegalArgumentException("Not enough chips!");
         }
         chips -= amount;
     }
     public void win(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Winning amount cannot be negative!");
+        }
         chips += amount;
     }
     public void setCurBet(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Bet amount cannot be negative!");
+        }
         curBet = amount;
     }
     public int getCurBet() {
         return curBet;
+    }
+    public void resetCurBet() {
+        curBet = 0;
     }
 }
